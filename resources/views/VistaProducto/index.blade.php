02808a2755355f910aa1f40b5810d6e1e0927ec3
YOFRANCITO <maisterloderyt@gmail.com>
2023-11-11 23:59:19 -0400

    xd 3

--- resources/views/VistaProducto/index.blade.php
+++ resources/views/VistaProducto/index.blade.php
@@ -158,7 +158,7 @@
                                         <td class="px-2 py-3 whitespace-no-wrap text-center">{{ $p->ubicacion }}</td>
 
                                         <td class="px-1 py-1| whitespace-no-wrap">
-                                            @can('VistaProducto.edit')
+                                            {{-- @can('VistaProducto.edit') --}}
                                                 <a href="{{ route('producto.edit', $p->id) }}"
                                                     class="py-2 text-white hover:scale-125 transition-transform delay-75 flex items-center">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -167,10 +167,10 @@
                                                             d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                                     </svg>
                                                 </a>
-                                            @endcan
+                                            {{-- @endcan --}}
                                         </td>
                                         <td class="px-1 py-4 whitespace-no-wrap">
-                                            @can('VistaProducto.detroy')
+                                            {{-- @can('VistaProducto.detroy') --}}
                                                 <form action="{{ route('producto.destroy', $p->id) }}" method="POST">
                                                     @csrf
                                                     @method('DELETE')
@@ -184,7 +184,7 @@
                                                         </svg>
                                                     </button>
                                                 </form>
-                                            @endcan
+                                            {{-- @endcan --}}
                                         </td>
                                     </tr>
                                 @endforeach
@@ -216,7 +216,7 @@
                                         </td>
                                         <td class="px-2 py-1 whitespace-no-wrap text-center">{{ $p->ubicacion }}</td>
                                         <td class="px-1 py-1 whitespace-no-wrap">
-                                            @can('VistaProducto.edit')
+                                            {{-- @can('VistaProducto.edit') --}}
                                                 <a href="{{ route('producto.edit', $p->id) }}"
                                                     class="py-2 text-white hover:scale-125 transition-transform delay-75 flex items-center">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -226,10 +226,10 @@
                                                             d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                                     </svg>
                                                 </a>
-                                            @endcan
+                                            {{-- @endcan --}}
                                         </td>
                                         <td class="px-1 py-1 whitespace-no-wrap">
-                                            @can('VistaProducto.detroy')
+                                            {{-- @can('VistaProducto.detroy') --}}
                                                 <form action="{{ route('producto.destroy', $p->id) }}" method="POST">
                                                     @csrf
                                                     @method('DELETE')
@@ -243,7 +243,7 @@
                                                         </svg>
                                                     </button>
                                                 </form>
-                                            @endcan
+                                            {{-- @endcan --}}
                                         </td>
                                     </tr>
                                 @endforeach
