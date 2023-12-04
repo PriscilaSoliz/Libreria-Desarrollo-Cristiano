0555a38afb87a7f2100737415f90ef5ab89817d8
Daniel_Castedo <pablodaniel280403@gmail.com>
2023-12-04 18:49:56 -0400

    imagenes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,14 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+
+
         </x-slot>
-        <div class="w-64 h-64 bg-white flex items-center">
-            <img src="{{ asset("img/Logito.png") }}" alt="Descripción de la imagen" class="max-w-full max-h-full ml-20 rounded-md">
+        <div class="w-64 h-64 bg-white flex justify-center items-center sm:items-start sm:justify-start sm:ml-0 md:mr-20 lg:ml-10">
+            <img src="{{ asset('img/newlogo.png') }}" alt="Descripción de la imagen" class="max-w-full max-h-full rounded-md">
         </div>
+
+
         <x-validation-errors class="mb-4" />
 
         @if (session('status'))
@@ -18,8 +21,8 @@
             @csrf
 
             <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('Correo')" required autofocus autocomplete="username" />
+                <x-label for="email" value="{{ __('Correo') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('Email')" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
@@ -34,17 +37,18 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <<div class="flex items-center justify-end mt-4">
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrar</a>
+                @endif
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('') }}
                     </a>
                 @endif
-
                 <x-button class="ml-4">
                     {{ __('Iniciar') }}
                 </x-button>
-
             </div>
         </form>
     </x-authentication-card>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
-<x-guest-layout >
-    <x-authentication-card >
-        <x-slot name="logo" >
-            <x-authentication-card-logo />
+<x-guest-layout>
+    <x-authentication-card>
+        <x-slot name="logo">
+            {{-- <x-authentication-card-logo /> --}}
         </x-slot>
-        <div class="w-64 h-64 bg-white flex items-center">
-            <img src="{{ asset('img/Logito.png') }}" alt="Descripción de la imagen" class="max-w-full max-h-full ml-20 rounded-md">
+        <div class="w-64 h-64 bg-white flex justify-center items-center mx-auto">
+            <img src="{{ asset('img/newlogo.png') }}" alt="Descripción de la imagen" class="max-w-full max-h-full rounded-md">
         </div>
+
+
+
+
         <x-validation-errors class="mb-4" />
 
         @if (session('status'))
@@ -14,17 +18,19 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}" >
+        <form method="POST" action="{{ route('login') }}">
             @csrf
 
             <div>
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('Correo')"
+                    required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Contraseña') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -36,10 +42,12 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrar</a>
+                    <a href="{{ route('register') }}"
+                        class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrar</a>
                 @endif
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('password.request') }}">
                         {{ __('') }}
                     </a>
                 @endif
